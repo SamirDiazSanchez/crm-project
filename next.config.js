ec97a2c4e37fdccc8558f8bc4de69792d43131e1
Samir Diaz <samir.diaz.1206@gmail.com>
2022-05-05 18:03:19 -0500

    se corrigio next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  basePath: isProd ? '' : '/crm-project/',
-  assetPrefix: isProd ? './' : '/crm-project/'
+  basePath: isProd ? '/crm-project/' : '',
+  assetPrefix: isProd ? './crm-project/' : ''
 }
