ab083e048cbf8e16ee6dfc7ebe6f92643b23e2f2
Samir Diaz <samir.diaz.1206@gmail.com>
2022-05-05 18:44:20 -0500

    se alimino prefix y se dejo basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-	assetPrefix: isProd ? './crm-project/' : '',
-	basePath: '/crm-project'
+	basePath: isProd ? '/crm-project' : '/'
 }
