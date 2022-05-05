7b2f21f4adb3e012f11b64acb122884581000570
Samir Diaz <samir.diaz.1206@gmail.com>
2022-05-05 18:31:31 -0500

    se corrigio error en next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-	assetsPrefix: isProd ? './crm-project/' : '',
-	basePath: '/crm-project'
+	assetPrefix: isProd ? './crm-project/' : '',
+	basePath: '/crm-project/'
 }
